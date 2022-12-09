22f107a6fcc3c280c2c7b8c66b20272c1ce0a00b
Szymon Duchniewicz <szymon.duchniewicz@avanade.com>
2022-12-09 23:17:41 +0100

    Add Copernicus script and scene.
    Signed-off-by: Szymon Duchniewicz <szymon.duchniewicz@avanade.com>

---
+++ Assets/Copernicus.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Copernicus : MonoBehaviour
+{
+    public GameObject telescopeObject;
+    public GameObject bookShelfObject;
+    public GameObject writingStandObject;
+    public GameObject toiletObject;
+    //[Label("Copernicus states")]
+    public bool isStanding = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (isStanding)
+        {
+            if (collision.gameObject == telescopeObject)
+            {
+                // Currently looking through the telescope
+            }
+        }
+    }
+}
